1c500fb715fbca7caa85738ba7dce0ff8a3707c5
Simran Sharma <sharmakammo13@gmail.com>
2025-06-24 17:13:28 +0530

    dsa solutions

--- src/main/java/com/sparklesimply/array/TraversalVariants.java
+++ src/main/java/com/sparklesimply/array/TraversalVariants.java
@@ -23,4 +23,28 @@ public class TraversalVariants {
         return candidate;
     }
 
+    /**
+     * Problem statement: Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+     * Time complexity: O(n)
+     * @param nums
+     * @return
+     */
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] trackForward = new int[n];
+        int[] trackBackward = new int[n];
+        int[] trackResult = new int[n];
+        trackForward[0] = 1;
+        trackBackward[n-1] = 1;
+        for(int i=1; i<n; i++) {
+            trackForward[i] = trackForward[i-1] * nums[i-1];
+        }
+        for(int i=n-2; i>=0; i--) {
+            trackBackward[i] = trackBackward[i+1] * nums[i+1];
+        }
+        for(int i=0; i<n; i++)
+            trackResult[i] = trackForward[i]*trackBackward[i];
+        return trackResult;
+    }
+
 }
