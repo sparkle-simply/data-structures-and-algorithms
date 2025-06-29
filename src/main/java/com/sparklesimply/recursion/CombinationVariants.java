5bb2f34842f74b3842c93363e37a3b99de4ae1ff
Simran Sharma <sharmakammo13@gmail.com>
2025-06-29 17:50:35 +0530

    dsa solutions

--- src/main/java/com/sparklesimply/recursion/CombinationVariants.java
+++ src/main/java/com/sparklesimply/recursion/CombinationVariants.java
@@ -107,4 +107,33 @@ public class CombinationVariants {
         }
         return true;
     }
+
+    /**
+     * Problem statement: Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+     * Each number in candidates may only be used once in the combination.
+     * Note: The solution set must not contain duplicate combinations.
+     * Time complexity: O(2^n)
+     * @param candidates
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(candidates);
+        backTrackCombination(result, new ArrayList<>(), 0, target, candidates);
+        return result;
+    }
+    private void backTrackCombination(List<List<Integer>> result, List<Integer> combination, int start, int target, int[] nums) {
+        if(target == 0) {
+            result.add(new ArrayList<>(combination));
+            return;
+        }
+        for(int i=start; i<nums.length; i++) {
+            if(i>start && nums[i] == nums[i-1]) continue;
+            if(nums[i] > target) break;
+            combination.add(nums[i]);
+            backTrackCombination(result, combination, i+1, target-nums[i], nums);
+            combination.remove(combination.size()-1);
+        }
+    }
 }
