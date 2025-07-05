daf4aef746305c56eef3f271c77949724a0ef672
Simran Sharma <sharmakammo13@gmail.com>
2025-07-05 15:33:06 +0530

    dsa solutions

--- src/main/java/com/sparklesimply/array/TraversalVariants.java
+++ src/main/java/com/sparklesimply/array/TraversalVariants.java
@@ -48,4 +48,27 @@ public class TraversalVariants {
             trackResult[i] = trackForward[i]*trackBackward[i];
         return trackResult;
     }
+
+    /**
+     * Problem statement: Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+     * Time complexity: O(n)
+     * @param nums
+     * @param k
+     */
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k%n;
+        reverseNums(nums, 0, n-1);
+        reverseNums(nums, 0, k-1);
+        reverseNums(nums, k, n-1);
+    }
+    private void reverseNums(int[] nums, int start, int end) {
+        while(start <= end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
 }
