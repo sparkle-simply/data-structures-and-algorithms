00db00efefa639df9c1fafe6f2b2408e31683482
Simran Sharma <sharmakammo13@gmail.com>
2026-04-03 09:57:52 +0530

    string dsa

--- src/main/java/com/sparklesimply/string/Miscellaneous.java
+++ src/main/java/com/sparklesimply/string/Miscellaneous.java
@@ -185,4 +185,33 @@ public class Miscellaneous {
         }
         return true;
     }
+
+    public static String stringConcatenation(String num1, String num2) {
+
+        if(num1.length() > num2.length()) {
+            String temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+        int n1 = num1.length();
+        int n2 = num2.length();
+        int diff = Math.abs(n2-n1);
+        String result = "";
+        int sum = 0,carry = 0;
+        for(int i=n1-1; i>=0; i--) {
+            sum = (int) ((num1.charAt(i) - '0') + (int) (num2.charAt(i+diff) - '0')) + carry;
+            result += (char) ((sum % 10) + '0');
+            carry = sum/10;
+        }
+        for(int i=diff-1; i>=0; i--) {
+            sum = (int) ((num2.charAt(i) - '0') + carry);
+            result += (char) ((sum % 10) + '0');
+            carry = sum/10;
+        }
+        if(carry > 0) {
+            result += (char) (carry + '0');
+        }
+        return new StringBuilder(result).reverse().toString();
+    }
+
 }
