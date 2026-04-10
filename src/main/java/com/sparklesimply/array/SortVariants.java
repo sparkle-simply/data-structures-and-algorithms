53b9a44386ddd6c696f6b306be23fd41fcb3bace
Simran Sharma <sharmakammo13@gmail.com>
2026-04-10 09:45:12 +0530

    data structures

--- src/main/java/com/sparklesimply/array/SortVariants.java
+++ src/main/java/com/sparklesimply/array/SortVariants.java
@@ -56,15 +56,25 @@ public class SortVariants {
         }
     }
 
+    /**
+     * Finding first positive using cyclic sort.
+     * Time complexity: O(n)
+     */
     public static int firstMissingPositiveUsingCyclicSort(int[] arr, int n) {
 		for(int i=0; i<n; i++) {
+            // if arr[i] is within the range [1, n] and arr[i]
+            // is not placed at (arr[i]-1)th index in arr
             while(arr[i]>=1 && arr[i]<n && arr[i] != arr[arr[i]-1])	{
+                // then swap arr[i] and arr[arr[i]-1] to
+                // place arr[i] to its corresponding index
                 int temp = arr[i];
                 arr[i] = arr[arr[i]-1];
                 arr[temp-1] = temp;
             }
         }
 
+        // If any number is not at its corresponding index
+        // then it is the missing number
 		for(int i=1; i<=n; i++) {
             if(i != arr[i-1])
                 return i;
