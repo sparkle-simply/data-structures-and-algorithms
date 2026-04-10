4f7899248af130391ba13ea137b4544cea0d907f
Simran Sharma <sharmakammo13@gmail.com>
2026-04-10 09:42:38 +0530

    data structures

--- src/main/java/com/sparklesimply/array/SortVariants.java
+++ src/main/java/com/sparklesimply/array/SortVariants.java
@@ -55,4 +55,20 @@ public class SortVariants {
             }
         }
     }
+
+    public static int firstMissingPositiveUsingCyclicSort(int[] arr, int n) {
+		for(int i=0; i<n; i++) {
+            while(arr[i]>=1 && arr[i]<n && arr[i] != arr[arr[i]-1])	{
+                int temp = arr[i];
+                arr[i] = arr[arr[i]-1];
+                arr[temp-1] = temp;
+            }
+        }
+
+		for(int i=1; i<=n; i++) {
+            if(i != arr[i-1])
+                return i;
+        }
+		return n+1;
+    }
 }
