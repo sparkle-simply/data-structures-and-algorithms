164b64dd2b0dfef0f570529f89642401d4470c5a
Simran Sharma <sharmakammo13@gmail.com>
2026-04-18 16:14:19 +0530

    data structures

--- src/main/java/com/sparklesimply/array/MatrixVariants.java
+++ src/main/java/com/sparklesimply/array/MatrixVariants.java
@@ -105,4 +105,39 @@ public class MatrixVariants {
             }
         }
     }
+
+    public static void rotateMatrix(ArrayList<ArrayList<Integer>> mat, int n, int m) {
+        int top = 0, bottom = n-1, left = 0, right = m-1;
+        while(top < bottom && left < right) {
+            int prev = mat.get(top+1).get(left);
+            for(int i=left; i<=right; i++) {
+                int temp = mat.get(top).get(i);
+                mat.get(top).set(i, prev);
+                prev = temp;
+            }
+            top++;
+            for(int i=top; i<=bottom; i++) {
+                int temp = mat.get(i).get(right);
+                mat.get(i).set(right, prev);
+                prev = temp;
+            }
+            right--;
+            if(top <= bottom) {
+                for(int i=right; i>=left; i--) {
+                    int temp = mat.get(bottom).get(i);
+                    mat.get(bottom).set(i, prev);
+                    prev = temp;
+                }
+                bottom--;
+            }
+            if(left <= right) {
+                for(int i=bottom; i>=top; i--) {
+                    int temp = mat.get(i).get(left);
+                    mat.get(i).set(left, prev);
+                    prev = temp;
+                }
+                left++;
+            }
+        }
+    }
 }
