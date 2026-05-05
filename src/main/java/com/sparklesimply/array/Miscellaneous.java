28730a24af141f8b97a109d1098fbd7ae14b8205
Simran Sharma <sharmakammo13@gmail.com>
2026-05-05 21:39:47 +0530

    data structures

--- src/main/java/com/sparklesimply/array/Miscellaneous.java
+++ src/main/java/com/sparklesimply/array/Miscellaneous.java
@@ -11,8 +11,6 @@ public class Miscellaneous {
      * Problem statement: You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
      * Return true if you can reach the last index, or false otherwise.
      * Time complexity: O(n)
-     * @param nums
-     * @return
      */
     public boolean canJump(int[] nums) {
         int n = nums.length;
